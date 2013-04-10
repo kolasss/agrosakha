6038da0d354956ff3f4ed8ac5e4611e02dc88302
kolasss <kolas.batman@gmail.com>
2013-04-10 14:31:49 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agro14::Application.config.secret_token = 'fb24768d99685999241b9c5b1fee3f5559cf0ab6b50e68e225a77b843a9cd11204740a6d496f30f044ed4a0a8c378f479f90862d69ef0177fa901814bf19c497'
