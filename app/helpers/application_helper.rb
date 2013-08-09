6c737aa7b335961ea672b2db45ee30454105962a
kolasss <kolas.batman@gmail.com>
2013-08-09 18:32:34 +1000

    changed design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 module ApplicationHelper
 
   def display_base_errors resource
@@ -12,4 +13,27 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def custom_format(time)
+    if time >= Date.today
+      "Сегодня"
+    elsif (time >= Date.yesterday and time <= Date.today)
+      "Вчера"
+    else
+      time.strftime("%d.%m.%y")
+    end
+  end
+
+  #   date = 
+  #   case 
+  #   # display today's date as "today"
+  #   when t >= Date.today then "today"
+  #   # display yesterday's date as 'Yesterday'
+  #   when (t >= Date.yesterday and t <= Date.today) then "yesterday"
+  #   # otherwise, display date: eg. Mon 4th June
+  #   else
+  #     t.strftime("%A #{t.day.ordinalize} %B")
+  #   end
+  #   time = t.strftime(" #{t.strftime("%I").to_i}:%M%p").downcase
+  #   date + time
+  # end
 end
