faf2a85fc1ca989ce5d34d27c9aebe58057f2caa
kolasss <kolas.batman@gmail.com>
2013-09-06 13:34:54 +1000

    added google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,4 +61,16 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def google_analytics_js
+    content_tag(:script, :type => 'text/javascript') do
+      raw "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-39862520-2', 'agrosakha.ru');
+      ga('send', 'pageview');"
+    end if Rails.env.production?
+  end
 end
